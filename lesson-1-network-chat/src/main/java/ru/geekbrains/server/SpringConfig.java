4d3489b009e5c04550266458ee149bf0ad110467
Johnny3kk <some@mail.ru>
2020-08-20 15:24:11 +0300

    3 - Beans in SpringConfig.java kung-fu stile

---
+++ lesson-1-network-chat/src/main/java/ru/geekbrains/server/SpringConfig.java
@@ -0,0 +1,40 @@
+package ru.geekbrains.server;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import ru.geekbrains.server.auth.AuthService;
+import ru.geekbrains.server.auth.AuthServiceJdbcImpl;
+import ru.geekbrains.server.persistance.UserRepository;
+
+import javax.sql.DataSource;
+import java.sql.SQLException;
+
+@Configuration
+public class SpringConfig {
+
+    @Bean
+    public ChatServer chatServer(AuthService authService) {
+        return new ChatServer(authService);
+    }
+
+    @Bean
+    public AuthService authService(UserRepository userRepository) {
+        return new AuthServiceJdbcImpl(userRepository);
+    }
+
+    @Bean
+    public UserRepository userRepository(DataSource dataSource) throws SQLException {
+        return new UserRepository(dataSource);
+    }
+
+    @Bean
+    public DataSource dataSource() {
+        DriverManagerDataSource ds = new DriverManagerDataSource();
+        ds.setDriverClassName("com.mysql.cj.jdbc.Driver");
+        ds.setUsername("root");
+        ds.setPassword("waagh!");
+        ds.setUrl("jdbc:mysql://localhost:3306/network_chat?useUnicode=true&serverTimezone=UTC");
+        return ds;
+    }
+}
