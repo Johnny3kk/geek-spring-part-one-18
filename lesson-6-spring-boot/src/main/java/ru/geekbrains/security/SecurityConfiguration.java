1018d7a5aab911eefd9392137963fa0a40c103dc
Johnny3kk <some@mail.ru>
2020-10-15 01:09:39 +0300

    Lesson 8 - start

---
+++ lesson-6-spring-boot/src/main/java/ru/geekbrains/security/SecurityConfiguration.java
@@ -0,0 +1,9 @@
+package ru.geekbrains.security;
+
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+@EnableWebSecurity
+public class SecurityConfiguration {
+
+    
+}
